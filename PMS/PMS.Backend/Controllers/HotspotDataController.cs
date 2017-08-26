c147bf1c35b146b66f1d7d882b0926d3d02a2a46
Tomáš Kučečka <kuto@erni.sk>
2017-08-26 19:09:20 +0200

    Added HotspotData

--- PMS/PMS.Backend/Controllers/HotspotDataController.cs
+++ PMS/PMS.Backend/Controllers/HotspotDataController.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PMS.Backend.Mocks;
+using PMS.Backend.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace PMS.Backend.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class HotspotDataController : Controller
     {
-        // GET api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public JsonResult Get(Guid id)
         {
-            return "value";
+            var hotspotData = HotspotDataMocks.HotspotData.Where(d => d.HotspotId == id);
+            if (hotspotData == null)
+                return Json(Enumerable.Empty<HotspotData>());
+
+            return Json(HotspotDataMocks.HotspotData);
         }
 
         // POST api/values
---
+++ PMS/PMS.Backend/Model/HotspotData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PMS.Backend.Model
+{
+    public class HotspotData
+    {
+        public int Id { get; set; }
+
+        public Guid HotspotId { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public double Pm2_5 { get; set; }
+
+        public double Pm10 { get; set; }
+        
+        public double Humidity { get; set; }
+
+        public double Temperature { get; set; }
+    }
+}
