ab74d394bc8424adde38a0358603dc9659c271f5
Michal Dorner <dorner.michal@gmail.com>
2017-08-26 19:41:36 +0200

    Add  MobileClient & MobileClient.Android with Map control

---
+++ PMS/PMS.MobileClient.Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+
+namespace PMS.MobileClient.Android
+{
+    [Activity(Label = "PMS.MobileClient.Android", Theme = "@style/MainTheme", MainLauncher = true)]
+    public class MainActivity : FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.FormsMaps.Init(this, bundle);
+            LoadApplication(new MobileClientApp());
+
+        }
+    }
+}
+
